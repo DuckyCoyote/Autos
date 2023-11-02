e8790bc7dbd9f016ee6de49270e9f31df595e009
Alejandro Cruz <daniel_719021@hotmail.es>
2023-11-02 02:36:45 -0600

    form-control1

--- client/src/pages/PostAuto/PostAuto.jsx
+++ client/src/pages/PostAuto/PostAuto.jsx
@@ -1,34 +1,65 @@
-import axios from 'axios';
+import axios from "axios";
 
 const PostAuto = () => {
-
-  const submitHandler = () => {
-    data
-  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Enviando..");
+  };
 
   return (
-    <div className='container'>
-      <form onSubmit={submitHandler}>
+    <div className="container">
+      <form onSubmit={(e) => submitHandler(e)}>
         <div className="mb-3">
-          <label for="placas" className="form-label">Placas</label>
-          <input type="text" className="form-control" id="placas" aria-describedby="Ej.P90IOS" placeholder="Ej.d90iuo" />
-        </div>
-        <div class="mb-3">
-          <label for="marca" className="form-label">Marca</label>
-          <input type="text" className="form-control" id="marca" placeholder="Ej.Chevrolet" />
+          <label htmlFor="placas" className="form-label">
+            Placas
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="placas"
+            aria-describedby="Ej.P90IOS"
+            placeholder="Ej.d90iuo"
+          />
         </div>
         <div className="mb-3">
-          <label for="year" className="form-label">Año</label>
-          <input type="number" className="form-control" id="year" placeholder="Ej.d90iuo" />
+          <label htmlFor="marca" className="form-label">
+            Marca
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="marca"
+            placeholder="Ej.Chevrolet"
+          />
         </div>
-        <div class="mb-3">
-          <label for="modelo" className="form-label">Modelo</label>
-          <input type="text" className="form-control" id="modelo" placeholder="Ej.Cavalier" />
+        <div className="mb-3">
+          <label htmlFor="year" className="form-label">
+            Año
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="year"
+            placeholder="Ej.d90iuo"
+          />
         </div>
-        <button type="submit" className="btn btn-primary">Enviar</button>
+        <div className="mb-3">
+          <label htmlFor="modelo" className="form-label">
+            Modelo
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="modelo"
+            placeholder="Ej.Cavalier"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default PostAuto
+export default PostAuto;
